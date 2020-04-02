d399d54c592202d42d508f3740c197df82150b0d
Mochammad Ifan Rizky Kurniawan <rizkykurniawan.ifan463@gmail.com>
2020-04-02 11:44:12 +0700

    add Validate Username and Password in MainActivity.java

--- app/src/main/java/com/example/exerciemenu/MainActivity.java
+++ app/src/main/java/com/example/exerciemenu/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.exerciemenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String user, pass;
+    EditText edNama, edPass;
+    Button bKirim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edNama = findViewById(R.id.editNama);
+        edPass = findViewById(R.id.editPass);
+        bKirim= findViewById(R.id.btnSign);
+
+        bKirim.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public  void onClick (View v) {
+                //Membaca input dari editText
+                user = edNama.getText().toString();
+                pass = edPass.getText().toString();
+
+                //Menampilkan popup
+                if (user.equals("admin") && (pass.equals("123")))  {
+                    Toast.makeText(MainActivity.this,
+                            "Login Sukses", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Password Salah", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
